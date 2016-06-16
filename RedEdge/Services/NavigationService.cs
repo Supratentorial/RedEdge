22fb94f48a69f986542c20b8b8b2cf526b41d582
Blake Mumford <blake.mumford@indoctrinate.com.au>
2016-06-17 07:51:33 +1000

    NavigationService

---
+++ RedEdge/Services/NavigationService.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedEdge.Services
+{
+    public class NavigationService : INavigationService
+    {
+        public string CurrentPageKey
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(string pageKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(string pageKey, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
