5c905096cbea17660e1aa2eb1728f2391450f615
Blake Mumford <blake.mumford@indoctrinate.com.au>
2016-07-06 08:15:00 +1000

    Working on add contact page

--- RedEdge.Desktop/ViewModels/AddContactViewModel.cs
+++ RedEdge.Desktop/ViewModels/AddContactViewModel.cs
@@ -1,9 +1,18 @@
-﻿using Template10.Mvvm;
+﻿using System.Collections.ObjectModel;
+using Template10.Mvvm;
 
 namespace RedEdge.Desktop.ViewModels
 {
     public class AddContactViewModel :ViewModelBase
     {
+        public ObservableCollection<string> TitleOptions { get; set; }
 
+        public AddContactViewModel()
+        {
+            TitleOptions = new ObservableCollection<string>()
+            {
+                "Mr", "Mrs", "Ms", "Miss", "Master", "Doctor", "Sir", "Professor"
+            };
+        }
     }
 }
